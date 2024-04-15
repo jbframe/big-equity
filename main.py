5b80bc1a61e564382313177fee32918d1189178d
John Frame <framejb@gmail.com>
2024-04-15 03:14:25 -0600

    main

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 from simulation import simulate_board
 
-# Example usage for various board stages:
+
 hero_hand = ['ad', 'ac', 'qd', 'jc', '2d']
 villain_hand = ['as', '2c', '5c', '7s', '10s']
 
 # No board cards
 board=[]
+print(f'hero_hand: {hero_hand}')
+print(f'villain_hand: {villain_hand}')
 print(f'board: {board}')
 simulate_board(hero_hand, villain_hand, board)
 
 
-# # Example usage for various board stages:
-# hero_hand = ['ad', 'ac', 'qd', 'jc', 'jd']
-# villain_hand = ['as', '2c', '9c', '9s', '10s']
+hero_hand = ['ad', 'ac', 'qd', '2d']
+villain_hand = ['as', '2c', '5c', '7s']
 
-# # No board cards
-# board=[]
-# print(f'board: {board}')
-# simulate_board(hero_hand, villain_hand, board)
+# No board cards
+board=[]
+print(f'hero_hand: {hero_hand}')
+print(f'villain_hand: {villain_hand}')
+print(f'board: {board}')
+simulate_board(hero_hand, villain_hand, board)
+
+hero_hand = ['ad', 'ac', 'qd', '2d']
+villain_hand = ['as', '2c', '5c', '10s']
+
+# No board cards
+board=[]
+print(f'hero_hand: {hero_hand}')
+print(f'villain_hand: {villain_hand}')
+print(f'board: {board}')
+simulate_board(hero_hand, villain_hand, board)
+
+
+hero_hand = ['ad', 'ac', 'qd', '2d']
+villain_hand = ['as', '2c', '9c', '10s']
+
+# No board cards
+board=[]
+print(f'hero_hand: {hero_hand}')
+print(f'villain_hand: {villain_hand}')
+print(f'board: {board}')
+simulate_board(hero_hand, villain_hand, board)
